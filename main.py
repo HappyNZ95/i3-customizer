5772d5651a14b44d73138ba90f7de6e7184e92b1
HappyNZ95 <htewini@gmail.com>
2024-10-29 10:59:15 +1300

    update colour of client_focused

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="A simple CLI program to customize the colours in i3wm.")
-
+i3_config_file_path = os.path.expanduser("~/.config/i3/config")
 # ArgumentParser
 
 parser.add_argument("colour", type=str, help="the colour you want")
@@ -10,17 +11,25 @@ args = parser.parse_args()
 
 client_focused_line = ""
 
+new_file_text = ""
+
 if args.colour == "blue":
 
     print("You selected the colour blue!\n\n")
 
-    original_file = open("/home/hayden/.config/i3/config", "r")
+    original_file = open(i3_config_file_path, "r")
 
     lines = original_file.readlines()
+    #for index, line in enumerate(lines):
     for line in lines:
         
-        if "client.focused" in line:
-            lines[lines.index(line)] = "client.focused #0096FF #0096FF #000000 #0096FF"
+        if "client.focused" in line and "_inactive" not in line:
+            line = "client.focused           #0096FF #0096FF #000000 #0096FF\n"
 
-    print(lines)
-    file.close()
+        new_file_text += line
+    
+    original_file.close()
+
+    new_file = open(i3_config_file_path, "w")
+    new_file.write(new_file_text)
+    new_file.close()
